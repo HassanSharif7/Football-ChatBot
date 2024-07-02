3beb4fc4a274a8e590fae3b7a6c9594b37b0ced1
Hassan <158609442+HassanSharif7@users.noreply.github.com>
2024-07-02 09:47:52 +0500

    Add files via upload

---
+++ Nltk.py
@@ -0,0 +1,28 @@
+
+
+import numpy as np
+from nltk.stem.porter import PorterStemmer
+stemmer=PorterStemmer()
+import nltk
+def tokenize(sentence):
+    return nltk.word_tokenize(sentence)
+
+def stem(word):
+    return  stemmer.stem(word.lower())
+
+
+def bag_of_words(tokenized_sentence,all_words):
+    tokenized_sentence= [stem(w) for w in  tokenized_sentence]
+    bag=np.zeros(len(all_words),dtype=np.float32)
+    for idx,w in enumerate(all_words):
+        if w in tokenized_sentence:
+            bag[idx]=1
+    return bag
+
+sentence=["hello","how","are","you"]
+words=["hi","hello","I","you","bye","thank","cool"]
+bag=bag_of_words(sentence,words)
+print(bag)
+
+
+
